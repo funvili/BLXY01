76c23206e99406f8771ec825ceb3ee3cf0ee4993
funvili <75257474+funvili@users.noreply.github.com>
2022-10-16 17:52:29 -0400

    Add files via upload

---
+++ BLXY Items/BLXY01/CharacterBody3d.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var rot_speed = 5
+@onready var spring_arm = $SpringArm3d
+@onready var model  = $CsgBox3d
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	if Vector2(velocity.z, velocity.x).length() > 0.2:
+		var look_direction = Vector2(velocity.z, velocity.x)
+		model.rotation.y = lerp_angle(model.rotation.y, look_direction.angle(), delta * rot_speed)
+
+	move_and_slide()
